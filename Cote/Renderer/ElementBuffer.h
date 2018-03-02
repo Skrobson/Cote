7d2186b04bcb3699b936ae68323d571f3f8520b5
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2018-03-02 10:37:35 +0100

    delete unused templated setData()

--- Cote/Renderer/ElementBuffer.h
+++ Cote/Renderer/ElementBuffer.h
@@ -4,31 +4,19 @@
 #include <memory>
 #include "ABuffer.h"
 namespace cote
-{namespace graphic 
 {
-	class ElementBuffer:public ABuffer
+	namespace graphic 
 	{
-	public:
-		ElementBuffer();
+		class ElementBuffer :public ABuffer
+		{
+		public:
+			ElementBuffer();
 
-		void bind()const noexcept override;
-		void unbind()const noexcept override;
-		
-		void setData(size_t count, const unsigned* data);
-		
-		template<typename RandomIt>
-		void setData(RandomIt beg, RandomIt end);
-	};
+			void bind()const noexcept override;
+			void unbind()const noexcept override;
 
-
-
-	template<typename RandomIt>
-	inline void ElementBuffer::setData(RandomIt beg, RandomIt end)
-	{
-		std::vector elem;
-		std::copy(beg, end, elem.begin());
-		setData(elem.size(), elem.data());
+			void setData(size_t count, const unsigned* data);
+		};
 	}
-}
 }
 
